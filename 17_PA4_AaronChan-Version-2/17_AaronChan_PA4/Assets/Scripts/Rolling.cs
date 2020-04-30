2901cb67abb3b6c2b9ab12509647dd588ba65b27
VANXEUS <diamonddialgabs@gmail.com>
2020-04-30 14:20:10 +0800

    Updated by 18_DennisHo
    Added MainScene

---
+++ 17_PA4_AaronChan-Version-2/17_AaronChan_PA4/Assets/Scripts/Rolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour
+{
+    public float MoveSpeed = 100;
+
+    private Rigidbody thisRigibody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigibody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 MoveX = Input.GetAxis("Horizontal") * Vector3.right;
+
+        Vector3 MoveZ = Input.GetAxis("Vertical") * Vector3.forward;
+
+        Vector3 MoveDirection = MoveX + MoveZ;
+
+        thisRigibody.AddForce(MoveDirection * MoveSpeed * Time.deltaTime);
+    }
+}
